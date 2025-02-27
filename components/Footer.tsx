0d8ba35576401dacfab96030f46ff7085bc889fe
Ray Santiago <150531595+Raysantiago07@users.noreply.github.com>
2025-02-27 10:08:12 -0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,11 +3,16 @@ import {View, Text, StyleSheet} from 'react-native'
 function Footer () {
   return (
     <View>
-      <Text>
+      <Text style={styles.margin}>
         2025 EEEP Deputado Roberto Mesquita
       </Text>
     </View>
   );
 }
-
+const styles = StyleSheet.create ({
+    margin: {
+      margin: 5,
+      padding: 5,
+    }  
+})
 export default Footer
